4aebc9240b3792e3dcd7993c09425b0476bf6d4f
nikhilsinghal1704 <nikhilsinghal1704@gmail.com>
2025-07-28 20:12:56 +0000

    change

--- SocialDashboard/settings.py
+++ SocialDashboard/settings.py
@@ -142,6 +142,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
